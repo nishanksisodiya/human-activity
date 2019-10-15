7ee8aebb2815c9c47bbde3dec542e6f44255b6fb
Nishank Singh Sisodiya <nishanksisodiya@gmail.com>
2019-10-15 14:23:52 +0530

    modified index

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('/index.html')
+
+@app.route('/uploadData', methods = ['GET', 'POST'])
+def uploadData():
+	if request.method == 'POST':
+		content = request.get_json();
+		with open('data.json', 'w', encoding='utf-8') as f:
+    		json.dump(content, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
