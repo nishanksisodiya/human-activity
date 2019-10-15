82847c12e7c22bf8dbef1cfbcf896e6a54254f8a
Nishank Singh Sisodiya <nishanksisodiya@gmail.com>
2019-10-15 14:09:42 +0530

    modified index

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,33 @@
-let status = document.getElementById('gyro');
+let gyro = document.getElementById('gyro');
+let accelero = document.getElementById('accl');
+
+setInterval(function()
+{
+	let gyroData = [];
+	$.ajax({
+		type:"post",
+		url:"myurl.html",
+		datatype:"html",
+		success:function(data)
+		{
+
+		}
+	});
+}, 3000);
 if ( 'Gyroscope' in window ) {
-	let gyro = new Gyroscope();
+	let gyro = new Gyroscope({ frequency: 50 });
 	gyro.addEventListener('reading', function(e) {
-		status.innerHTML = 'x: ' + e.target.x + '<br> y: ' + e.target.y + '<br> z: ' + e.target.z;
+		gyro.innerHTML = 'x: ' + e.target.x + '<br> y: ' + e.target.y + '<br> z: ' + e.target.z;
 	});
 	gyro.start();
 }
-else status.innerHTML = 'Gyroscope not supported';
+else gyro.innerHTML = 'Gyroscope not supported';
 
-let accelero = document.getElementById('accl');
 if ( 'Accelerometer' in window ) {
-	let accl = new Accelerometer();
-	accl.addEventListener('reading', function(e) {
+	let accel = new Accelerometer({ frequency: 50 });
+	accel.addEventListener('reading', function(e) {
 		accelero.innerHTML = 'x: ' + e.target.x + '<br> y: ' + e.target.y + '<br> z: ' + e.target.z;
 	});
-	accl.start();
+	accel.start();
 }
 else accelero.innerHTML = 'Accelerometer not supported';
