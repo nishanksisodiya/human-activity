be826c689b8bc545955d4de8a68b328346763528
Nishank Singh Sisodiya <nishanksisodiya@gmail.com>
2019-10-31 18:01:13 +0530

    v20

--- static/js/script.js
+++ static/js/script.js
@@ -157,7 +157,7 @@ function showArray()
 	console.log('[');
 	for (var i = 0; i < 80; i++) {
 		console.log('[');
-		for (var j = 0; i < 3; i++) 
+		for (var j = 0; j < 3; j++) 
 		{
 			console.log(accelArray[i][j] + ',');
 		}
